9691be1d42fac3ca6b445e167d57386417bd2387
Artem Sukhanov <IceMan161@yandex.ru>
2022-03-15 18:15:44 +0300

    TestF

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,7 +7,7 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain() {
+    public void shouldTellAmount() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
@@ -15,7 +15,32 @@ public class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
 
-        assertEquals(actual,expected);
-
+        assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldNotIterfere() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldTellAmount2() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
+
+
 }
