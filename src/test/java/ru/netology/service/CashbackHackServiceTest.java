6814b12088084420010da900af9d9a651dd364ba
Artem Sukhanov <IceMan161@yandex.ru>
2022-03-15 18:18:10 +0300

    TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -18,4 +18,28 @@ public class CashbackHackServiceTest {
         assertEquals(actual,expected);
 
     }
+
+    @Test
+    public void shouldNotIterfere() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldTellAmount2() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
 }
